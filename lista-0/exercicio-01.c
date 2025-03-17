cf4678967194196e0b00af232b04ac3a7ded5edb
cairuga <62620754+cairuga@users.noreply.github.com>
2025-03-17 09:37:59 -0300

    Update exercicio-01.c

--- lista-0/exercicio-01.c
+++ lista-0/exercicio-01.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+
+    int idade;
+    float peso;
+
+    printf("Informe a idade do atleta: ");
+    scanf("%d", &idade);
+
+    printf("Informe o peso do atleta: ");
+    scanf("%f", &peso);
+
+    if (idade < 12) {
+        if (peso < 40) printf("Infantil Leve");
+        else printf("Infantil Pesado");
+    } else if (idade < 18) {
+        if (peso < 60) printf("Juvenil Leve");
+        else printf("Juvenil Pesado");
+    } else {
+        if (peso < 75) printf("Adulto Leve");
+        else if (peso < 90) printf("Adulto Médio");
+        else printf("Adulto Pesado");
+    }
+
+    return 0;
+}
