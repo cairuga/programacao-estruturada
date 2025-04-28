d5679cb6b5a61672d8e84cd050c194fabbccd58d
cairuga <62620754+cairuga@users.noreply.github.com>
2025-04-28 15:40:06 -0300

    Create exercicio-11.c

---
+++ lista-02/exercicio-11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void imprimir_invertido(int vetor[], int tamanho) {
+    for (int i = tamanho - 1; i >= 0; i--) {
+        printf("%d ", vetor[i]);
+    }
+}
+
+int main() {
+    int quantidade;
+    printf("Quantos números você deseja informar? ");
+    scanf("%d", &quantidade);
+    int numeros[quantidade];
+    for (int i = 0; i < quantidade; i++) {
+        printf("Informe o %dº número: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+    printf("Os números informados em ordem inversa são: ");
+    imprimir_invertido(numeros, quantidade);
+}
