580accb6f83e5fbc8da0899c041fdcf1ae68da61
cairuga <62620754+cairuga@users.noreply.github.com>
2025-04-28 15:15:12 -0300

    Create exercicio-03.c

---
+++ lista-02/exercicio-03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float media(float valor1, float valor2) {
+    return (valor1 + valor2) / 2;
+}
+
+int main() {
+    float valor1, valor2;
+    printf("Digite o primeiro valor: ");
+    scanf("%f", &valor1);
+    printf("Digite o segundo valor: ");
+    scanf("%f", &valor2);
+    printf("A média é %f.", media(valor1, valor2));
+}
