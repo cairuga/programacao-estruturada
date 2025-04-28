ff2068ce6c492175c51d66d5892fb84058cc363b
cairuga <62620754+cairuga@users.noreply.github.com>
2025-04-28 15:09:22 -0300

    Create exercicio-01.c

---
+++ lista-02/exercicio-01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int dobro(int valor) {
+    return valor * 2;
+}
+
+int main() {
+    int numero;
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+    printf("O dobro de %d é %d.", numero, dobro(numero));
+}
